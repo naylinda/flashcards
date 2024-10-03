cb3d37c2c7cf8ee8f2e88bff42e27d5c52a1e5ea
naylinda <148217113+naylinda@users.noreply.github.com>
2024-10-03 20:51:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+const perguntas = [
+    {
+        pergunta: 'Qual foi o primeiro modelo de iPhone lançado?',
+        resposta: 'O primeiro iPhone foi lançado em 29 de junho de 2007.'
+    },
+    {
+        pergunta: 'Qual é a principal funcionalidade do Face ID?',
+        resposta: 'O Face ID utiliza reconhecimento facial para desbloquear o iPhone e autenticar compras.'
+    },
+    {
+        pergunta: 'O que é o iOS?',
+        resposta: 'iOS é o sistema operacional desenvolvido pela Apple para seus dispositivos móveis, incluindo o iPhone.'
+    },
+    {
+        pergunta: 'Qual é a capacidade máxima de armazenamento do iPhone 14?',
+        resposta: 'O iPhone 14 oferece opções de armazenamento de até 1TB.'
+    },
+    {
+        pergunta: 'Quais são as cores disponíveis do iPhone 13?',
+        resposta: 'O iPhone 13 está disponível nas cores azul, rosa, vermelho, meia-noite, estelar e verde.'
+    },
+    {
+        pergunta: 'O que é o MagSafe?',
+        resposta: 'O MagSafe é um sistema de magnetismo que permite acessórios e carregadores se fixarem ao iPhone facilmente.'
+    },
+    {
+        pergunta: 'Qual é a duração da bateria do iPhone 12?',
+        resposta: 'O iPhone 12 tem uma duração de bateria de até 17 horas em uso contínuo.'
+    },
+    {
+        pergunta: 'O que é o Apple Pay?',
+        resposta: 'Apple Pay é um serviço de pagamento móvel que permite fazer compras usando o iPhone.'
+    },
+    {
+        pergunta: 'Como se chama a assistente virtual do iPhone?',
+        resposta: 'A assistente virtual do iPhone se chama Siri.'
+    },
+    {
+        pergunta: 'O que é a App Store?',
+        resposta: 'A App Store é a loja de aplicativos da Apple, onde os usuários podem baixar aplicativos para o iPhone.'
+    }
+];
